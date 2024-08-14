98d7cebaad38f43b4b27eebcfd20b5b8f83f097a
Vitor Hugo <107222031+VitorHugoOliveiraBarbosa@users.noreply.github.com>
2024-08-14 00:22:59 +0000

    preparação da dependencias

---
+++ projeto01/src/app/component-pai/component-pai.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component-pai',
+  standalone: true,
+  imports: [],
+  templateUrl: './component-pai.component.html',
+  styleUrl: './component-pai.component.css'
+})
+export class ComponentPaiComponent {
+
+}
