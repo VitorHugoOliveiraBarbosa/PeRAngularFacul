65b421cec4cd02d135e665f16efecd92e39d31f1
Vitor Hugo Oliveira <107222031+VitorHugoOliveiraBarbosa@users.noreply.github.com>
2024-08-28 20:38:07 -0300

    começando o projeto primeiro-app

--- primeiro-app/src/app/app.component.spec.ts
+++ primeiro-app/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'projeto01' title`, () => {
+  it(`should have as title 'primeiro-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('projeto01');
+    expect(app.title).toEqual('primeiro-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, projeto01');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('primeiro-app app is running!');
   });
 });
