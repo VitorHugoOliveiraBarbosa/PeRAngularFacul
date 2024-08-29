10c583c0732a5e23c2f674410b2139cff81950a7
Vitor Hugo <107222031+VitorHugoOliveiraBarbosa@users.noreply.github.com>
2024-08-29 23:39:59 +0000

    usando dataBind

--- primeiro-app/src/app/app-routing.module.ts
+++ primeiro-app/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientListComponent } from './pages/client-list/client-list.component';
+import { HoursListComponent } from './pages/hours-list/hours-list.component';
 
 const routes: Routes = [
-  {path:'Client', component: ClientListComponent}
+  {path:'Client', component: ClientListComponent},
+  {path:'Hours', component: HoursListComponent}
 ];
 
 @NgModule({
